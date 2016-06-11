b51f09f490537a61a70cff3589c012a749088e2e
Troy Uyan <blutank24@msn.com>
2016-06-11 00:00:18 -0700

    AC apps

---
+++ Forks/assettocorsa_apps/apps/python/fuel_usage/fuel_usage.py
@@ -0,0 +1,252 @@
+################################################################################
+# Fuel Usage App for Assetto Corsa by Thomas Gocke
+# https://github.com/flitzi/AC_APPS
+# 
+# Shows from left to right  -the fuel left in tank  -average fuel per lap  -laps left until tank is empty 
+# The out lap is skipped, so you need to drive 2 laps before calculation starts.
+# The average fuel usage per lap is stored in C:/Users/[YourUserName]/AC_fuel_usage/[CarName]_[TrackName].ini
+# The stored value (if existing) is used for the first two laps until the new calculation is started
+#
+# Changelog:
+#
+# 1.0 
+# - first release
+#
+# 1.1
+# - added average fuel consumption in liters per 100 km and inst. fuel consumption (also in liters per 100 km)
+# - fixed bug in average per lap calculation when removing fuel in pits (leaving pits with less fuel than at pit entry)
+#
+# 1.2
+# - fixed bug for game modes not starting in pit (hotlap, time attack)
+#
+# 1.3
+# - added miles per gallon calculation (change constant milesPerGallon to True)
+# - fixed liters per 100km calculation after restart and not starting in pit (distance travelled not resetted in AC?)
+#
+# 1.5
+# - when crossing the line, fuel consumption of last lap and remaining laps based on this value are shown for a couple of seconds (blinking)
+#
+# 1.6
+# - updated to AC 0.20 shared memory struct
+#
+# 1.7
+# - updated to AC 0.22 shared memory struct
+#
+# 1.8
+# - compatibility with track configurations 
+#
+# 1.9
+# - compatibility with AC 1.6 (thanks to B-RIDDICK)
+#
+################################################################################
+
+import string
+import math
+import ac
+import acsys
+import os.path
+import struct                          # for converting from bytes to useful data
+import mmap                            # for reading shared memory
+
+###directory for storing ini files, home("~") is C:/Users/[YourUserName]
+from os.path import expanduser
+inidir = expanduser("~") + "/AC_fuel_usage/"
+
+###valid file name characters
+validFilenameChars = "-_() abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+###appsize
+x_app_size = 300
+y_app_size = 66
+
+###startingpoint of first label
+x_start = 1
+y_start = 26
+
+################## CHANGE FOR MILES PER GALLON HERE #######
+milesPerGallon = False
+################## CHANGE FOR MILES PER GALLON HERE #######
+
+################## CHANGE BACKROUND HERE ##################
+background = 0
+################## CHANGE BACKROUND HERE ##################
+
+class Fuel_Usage:  
+
+  def __init__(self, app, x, y):
+    
+    self.AverageFuelPerLap = 0.0
+    self.FuelLastLap = 0.0
+    self.completedLaps = 0.0
+    self.fuelAtLapStart = 0.0
+    self.distanceTraveledAtStart = 0.0
+    self.fuelAtStart = 0.0
+    self.lastFuelMeasurement = 0.0
+    self.lastDistanceTraveled = 0.0
+    self.counter = 0
+    self.updatecounter = 0
+    
+    self.inifilepath = inidir + self.getValidFileName(ac.getCarName(0)) +"_" + self.getValidFileName(ac.getTrackName(0)) + self.getValidFileName(ac.getTrackConfiguration(0)) + ".ini"
+    
+    ##initialize labels
+    
+    self.remainingLabel = ac.addLabel(app, "remainingLabel")
+    self.averageFuelPerLapLabel = ac.addLabel(app, "averageFuelPerLapLabel")
+    self.lapsLeftLabel = ac.addLabel(app, "lapsLeftLabel")
+    self.averageFuelPer100kmLabel = ac.addLabel(app, "averageFuelPer100km")
+    self.instFuelLabel = ac.addLabel(app, "instFuel")
+    
+    ##set label positions
+    
+    ac.setPosition(self.remainingLabel, x, y)
+    ac.setPosition(self.averageFuelPerLapLabel, x + 208, y)
+    ac.setPosition(self.lapsLeftLabel, 150 - x, y)
+    ac.setPosition(self.averageFuelPer100kmLabel, x + 208, y + 19)
+    ac.setPosition(self.instFuelLabel, 150 - x, y + 19)
+    
+    ##set label alignments
+    
+    ac.setFontAlignment(self.remainingLabel, "left")
+    ac.setFontAlignment(self.averageFuelPerLapLabel, "left")
+    ac.setFontAlignment(self.lapsLeftLabel, "right")
+    ac.setFontAlignment(self.averageFuelPer100kmLabel, "left")
+    ac.setFontAlignment(self.instFuelLabel, "right")
+    
+    ##set font size
+    
+    ac.setFontSize(self.remainingLabel, 32)
+    ac.setFontSize(self.averageFuelPerLapLabel, 16)
+    ac.setFontSize(self.lapsLeftLabel, 16)
+    ac.setFontSize(self.averageFuelPer100kmLabel, 16)
+    ac.setFontSize(self.instFuelLabel, 16)
+    
+    if os.path.exists(self.inifilepath):
+      f = open(self.inifilepath, "r")
+      self.AverageFuelPerLap = float(f.readline()[6:])
+      f.close()
+    
+    ac.setText(self.remainingLabel, "--- l")
+    ac.setText(self.averageFuelPerLapLabel, "--- l/lap")
+    ac.setText(self.lapsLeftLabel, "--- laps")
+    if milesPerGallon:
+      ac.setText(self.averageFuelPer100kmLabel, "--- mpg")
+    else:
+      ac.setText(self.averageFuelPer100kmLabel, "--- l/100km")
+    ac.setText(self.instFuelLabel, "--- inst.")
+  
+  def getValidFileName(self, filename):
+    return "".join(c for c in filename if c in validFilenameChars)
+  
+  def lPer100kmToMPG(self, lp100km):
+    if lp100km == 0:
+      return 999.0
+    else:
+      return 235.214584 / lp100km
+  
+  def Update(self):
+    remaining = self.readFuel() #read remaining fuel from shared memory
+    completedLaps, distanceTraveled, isInPit = self.readInfo() #read completedLaps, distanceTraveled, isInPit from shared memory
+    
+    if isInPit == 1 or self.lastFuelMeasurement < remaining or self.lastFuelMeasurement - 2 > remaining: #if in pit or restarted (refilled fuel or taken fuel out)
+      self.fuelAtLapStart = 0 #set fuelAtLapStart to 0 so the coming lap will not be counted
+      self.fuelAtStart = remaining
+      self.distanceTraveledAtStart = distanceTraveled
+    
+    if completedLaps != self.completedLaps: #when crossed finish line
+      if completedLaps >= 2 and self.fuelAtLapStart > remaining: #if more than 2 laps driven
+        self.FuelLastLap = self.fuelAtLapStart - remaining # calculate fuel used last lap
+        self.AverageFuelPerLap = (self.AverageFuelPerLap * (completedLaps - 2) + self.FuelLastLap) / (completedLaps - 1) #calculate AverageFuelPerLap
+      self.fuelAtLapStart = remaining #reset fuelAtLapStart
+      self.completedLaps = completedLaps #set completedLaps
+      self.counter = 999
+      self.updatecounter = 0
+    
+    if self.counter > 30: #if enough ticks for update
+      ac.setText(self.remainingLabel, "{0} l".format(round(remaining, 1))) #set remaining fuel text
+      if self.AverageFuelPerLap > 0:
+        if self.updatecounter > 20 or self.FuelLastLap == 0:
+          ac.setText(self.averageFuelPerLapLabel, "{0} l/lap".format(round(self.AverageFuelPerLap, 2))) #set averageFuelPerLap text
+          ac.setText(self.lapsLeftLabel, "{0} laps".format(round(remaining / self.AverageFuelPerLap, 1))) #set lapsLeft text
+        else:
+          if self.updatecounter % 2 == 1:
+            ac.setText(self.averageFuelPerLapLabel, "{0} last".format(round(self.FuelLastLap, 2))) #set fuel last lap text
+            ac.setText(self.lapsLeftLabel, "{0} laps".format(round(remaining / self.FuelLastLap, 1))) #set lapsLeft with last lap consumption text
+          else:
+            ac.setText(self.averageFuelPerLapLabel, " ") #make text blinking
+            ac.setText(self.lapsLeftLabel, " ") #make text blinking
+      
+      distance = distanceTraveled - self.lastDistanceTraveled #distance since last calculation update
+      if distance > 0.01:
+        lp100km = (self.lastFuelMeasurement - remaining) / distance * 100000
+        if milesPerGallon:
+          ac.setText(self.instFuelLabel, "{0} inst".format(round(self.lPer100kmToMPG(lp100km), 1))) #set inst. fuel consumption text in mpg
+        else:
+          ac.setText(self.instFuelLabel, "{0} inst".format(round(lp100km, 1))) #set inst. fuel consumption text
+      else:
+        if milesPerGallon: 
+          ac.setText(self.instFuelLabel, "0 inst") #set inst. fuel consumption text if not moving in mpg
+        else:
+          ac.setText(self.instFuelLabel, "999 inst") #set inst. fuel consumption text if not moving
+      
+      distance = distanceTraveled - self.distanceTraveledAtStart #distance since last last pit
+      if distance > 0.01:
+        lp100km = (self.fuelAtStart - remaining) / distance * 100000
+        if milesPerGallon:
+          ac.setText(self.averageFuelPer100kmLabel, "{0} mgp".format(round(self.lPer100kmToMPG(lp100km), 1))) #set average fuel consumption text in mpg
+        else:
+          ac.setText(self.averageFuelPer100kmLabel, "{0} l/100km".format(round(lp100km, 1))) #set average fuel consumption text
+      else:
+        if milesPerGallon: 
+          ac.setText(self.averageFuelPer100kmLabel, "0 mpg") #set average fuel consumption text if not moving in mpg
+        else:
+          ac.setText(self.averageFuelPer100kmLabel, "999 l/100km") #set average fuel consumption text if not moving
+      
+      self.lastFuelMeasurement = remaining #reset lastFuelMeasurement
+      self.lastDistanceTraveled = distanceTraveled #reset lastDistanceTraveled
+      self.updatecounter = self.updatecounter + 1 #increment update counter
+      self.counter = 0 #reset counter
+    
+    self.counter = self.counter + 1 #increment counter
+  
+  def readFuel(self):
+    shmHandle = mmap.mmap(0, 256, "acpmf_physics")
+    shmHandle.seek(12)
+    data = shmHandle.read(4)
+    shmHandle.close()  
+    return struct.unpack("<f", data)[0]
+  
+  def readInfo(self):
+    shmHandle = mmap.mmap(0, 262, "acpmf_graphics")
+    shmHandle.seek(132)    
+    completedLaps = struct.unpack("<L", shmHandle.read(4))[0]
+    shmHandle.seek(156)    
+    distanceTraveled = struct.unpack("<f", shmHandle.read(4))[0]
+    isInPit = struct.unpack("<L", shmHandle.read(4))[0]
+    shmHandle.close()  
+    return completedLaps, distanceTraveled, isInPit
+
+def acMain(ac_version):
+  global fuelUsage
+  appWindow = ac.newApp("Fuel Usage")
+  ac.drawBackground(appWindow, background)  
+  ac.drawBorder(appWindow, background)
+  ac.setBackgroundOpacity(appWindow, background)
+  ac.setSize(appWindow, x_app_size, y_app_size)
+  fuelUsage = Fuel_Usage(appWindow, x_start, y_start)  
+  ac.addRenderCallback(appWindow, onFormRender)
+  
+  ac.log("Fuel Usage App loaded")
+  return "Fuel Usage App"
+
+def acShutdown():
+  global fuelUsage
+  if not os.path.exists(inidir):
+    os.makedirs(inidir)
+  if fuelUsage.AverageFuelPerLap > 0:
+    f = open(fuelUsage.inifilepath, "w")
+    f.write("l/lap={0}".format(fuelUsage.AverageFuelPerLap))
+    f.close()
+
+def onFormRender(deltaT):
+  global fuelUsage
+  fuelUsage.Update()
---
+++ Forks/assettocorsa_apps/apps/python/tyre_temp/tyre_temp.py
@@ -0,0 +1,421 @@
+################################################################################
+# Danny Giusa Tyre Temperatures
+# danny(dot)giusa(at)gmail(dot)com
+# V1.2, 06.12.2013, AC Early Access 0.3.0
+# 
+# This little app provides informations about the state of your
+# tyre like temperature, pressure and level of dirt
+#
+# Changelog:
+#
+# 1.0 
+# - restructure of the techpreview app
+# - added level of dirt with raising bar on tyrebar
+#
+# 1.1
+# - added max temps and psi recording
+# - rearrange dirtbars and value
+#
+# 1.2
+# - added max values
+# - fixed that under 25C drawn tyres disappear
+# - using hue of HSV color wheel for temperature coloring (flitzi)
+# - variables for optimal temperature and temperature range (flitzi)
+# - added spinner for adjusting the optimal temperature while in the car (flitzi)
+# - new alignment for the ui
+#
+# 1.2.1
+# - saving and loading optimal tyre temperature per car in C:/Users/[YourUserName]/AC_tyre_temp/ (flitzi)
+# - added variable for showing/hiding optimal temperature spinner (flitzi)
+#
+# 1.2.2
+# - only write optimal tyre temperature when changed (flitzi)
+# 
+# 1.2.3
+# - displaying tyre wear % instead of tyre dirt %, tyre dirt bar remains in place (flitzi)
+#
+# 1.2.4
+# - only showing optimal temperature spinner when in pit, it gets automatically hidden when leaving the pits (flitzi)
+#
+# 1.2.5
+# - inFahrenheit variable for changing temperature unit (ini file always stores the value in Celsius, only the GUI is changed to Fahrenheit) (flitzi)
+#
+# 1.2.6
+# - updated to AC 0.20 shared memory struct
+#
+# 1.2.7
+# - updated to AC 0.22 shared memory struct
+#
+################################################################################
+
+import string
+import math
+import ac
+import acsys
+import os.path
+import struct                          # for converting from bytes to useful data
+import mmap                            # for reading shared memory
+
+###directory for storing ini files, home("~") is C:/Users/[YourUserName]
+from os.path import expanduser
+inidir = expanduser("~") + "/AC_tyre_temp/"
+
+###valid file name characters
+validFilenameChars = "-_() abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+TYREINFO = 0
+###appsize
+x_app_size = 300
+y_app_size = 195
+###startingpoint of first label
+x_start = 17
+y_start = 40
+###spaces from left to right label/tyre and upper and bottom
+x_space = 215
+y_space = 75
+###spaces for drawn tyre 
+x_tyre_position = x_start + 53 
+y_tyre_position = y_start + 5
+###spaces for drawn tyre from left to right
+x_tyre_space = 80
+x_tyredirt_space = 3
+###settings for drawn tyre
+w_tyre = 23
+h_tyre = 50
+###settings for offset of drawn tyredirt
+w_tyredirt = w_tyre -6
+
+
+################## CHANGE BACKROUND HERE ##################
+background = 0
+################## CHANGE BACKROUND HERE ##################
+
+################## CHANGE TEMPERATURE UNIT HERE ##################
+# False for Celsius , True for Fahrenheit
+inFahrenheit = False
+################## CHANGE TEMPERATURE UNIT HERE ##################
+
+###d_optimal_temp is displayed as green
+###d_optimal_temp - temp_rage / 2 is displayed as blue
+###d_optimal_temp + temp_rage / 2 is displayed as red
+###if the optimal tyre temperature is set to 100 and the temperature range is 80 then 60 is displayed blue, 100 green and 140 red
+###the optimal temperature can be adjusted using the spinner
+d_optimal_temp = 100 
+temp_range = 80 
+
+class Tyre_Info:  
+
+  def __init__(self, app, x, y):
+    x_pressure = x
+    x_dirt = x_pressure
+    x_maxt = x + 80
+    x_maxp = x_maxt
+    
+    y_pressure = y + 20
+    y_dirt = y_pressure + 20
+    y_maxt = y + 8
+    y_maxp = y_maxt + 20
+    
+    self.maxtFL = 0
+    self.maxtFR = 0
+    self.maxtRL = 0
+    self.maxtRR = 0
+    
+    self.maxpFL = 0
+    self.maxpFR = 0
+    self.maxpRL = 0
+    self.maxpRR = 0
+    
+    self.unitChar = "C"
+    
+    if inFahrenheit:
+      self.unitChar = "F"
+    
+    ##initialize labels
+    
+    self.tFLValue = ac.addLabel(app, "temp fl")
+    self.tFRValue = ac.addLabel(app, "temp fr")
+    self.tRLValue = ac.addLabel(app, "temp rl")
+    self.tRRValue = ac.addLabel(app, "temp rr")
+    self.pFLValue = ac.addLabel(app, "psi fl")
+    self.pFRValue = ac.addLabel(app, "psi fr")
+    self.pRLValue = ac.addLabel(app, "psi rl")
+    self.pRRValue = ac.addLabel(app, "psi rr")
+    self.dFLValue = ac.addLabel(app, "dirt fl")
+    self.dFRValue = ac.addLabel(app, "dirt fr")
+    self.dRLValue = ac.addLabel(app, "dirt rl")
+    self.dRRValue = ac.addLabel(app, "dirt rr")
+    self.maxFont = ac.addLabel(app, "Max")
+    self.maxtFont = ac.addLabel(app, self.unitChar)
+    self.maxpFont = ac.addLabel(app, "psi")
+    self.maxtFontBottom = ac.addLabel(app, self.unitChar)
+    self.maxpFontBottom = ac.addLabel(app, "psi")
+    self.maxtFLValue = ac.addLabel(app, "maxtemp fl")
+    self.maxtFRValue = ac.addLabel(app, "maxtemp fr")
+    self.maxtRLValue = ac.addLabel(app, "maxtemp rl")
+    self.maxtRRValue = ac.addLabel(app, "maxtemp rr")
+    self.maxpFLValue = ac.addLabel(app, "maxpress fl")
+    self.maxpFRValue = ac.addLabel(app, "maxpress fr")
+    self.maxpRLValue = ac.addLabel(app, "maxpress rl")
+    self.maxpRRValue = ac.addLabel(app, "maxpress rr")
+    
+    ##set label positions
+    
+    ac.setPosition(self.tFLValue, x - 152, y)
+    ac.setPosition(self.tFRValue, x + x_space, y)
+    ac.setPosition(self.tRLValue, x - 152, y + y_space)
+    ac.setPosition(self.tRRValue, x + x_space, y + y_space)
+    ac.setPosition(self.pFLValue, x_pressure - 152, y_pressure)
+    ac.setPosition(self.pFRValue, x_pressure + x_space, y_pressure)
+    ac.setPosition(self.pRLValue, x_pressure - 152, y_pressure + y_space)
+    ac.setPosition(self.pRRValue, x_pressure + x_space, y_pressure + y_space)
+    ac.setPosition(self.dFLValue, x_dirt - 152, y_dirt)
+    ac.setPosition(self.dFRValue, x_dirt + x_space, y_dirt)
+    ac.setPosition(self.dRLValue, x_dirt - 152, y_dirt + y_space)
+    ac.setPosition(self.dRRValue, x_dirt + x_space, y_dirt + y_space)
+    ac.setPosition(self.maxFont, 137, 28)
+    ac.setPosition(self.maxtFont, 145, 48)
+    ac.setPosition(self.maxpFont, 140, 68)
+    ac.setPosition(self.maxtFontBottom, 145, 123)
+    ac.setPosition(self.maxpFontBottom, 140, 143)
+    ac.setPosition(self.maxtFLValue, x_maxt, y_maxt)
+    ac.setPosition(self.maxtFRValue, x_maxt - 96, y_maxt)
+    ac.setPosition(self.maxtRLValue, x_maxt, y_maxt + y_space)
+    ac.setPosition(self.maxtRRValue, x_maxt - 96, y_maxt + y_space)
+    ac.setPosition(self.maxpFLValue, x_maxp, y_maxp)
+    ac.setPosition(self.maxpFRValue, x_maxp - 96, y_maxp)
+    ac.setPosition(self.maxpRLValue, x_maxp, y_maxp + y_space)
+    ac.setPosition(self.maxpRRValue, x_maxp - 96, y_maxp + y_space)
+    
+    ##set label alignments
+    
+    ac.setFontAlignment(self.tFLValue, "right")
+    ac.setFontAlignment(self.tRLValue, "right")
+    ac.setFontAlignment(self.pFLValue, "right")
+    ac.setFontAlignment(self.pRLValue, "right")
+    ac.setFontAlignment(self.dFLValue, "right")
+    ac.setFontAlignment(self.dRLValue, "right")
+    ac.setFontAlignment(self.maxtFLValue, "left")    
+    ac.setFontAlignment(self.maxtFRValue, "right")
+    ac.setFontAlignment(self.maxtRLValue, "left")
+    ac.setFontAlignment(self.maxtRRValue, "right")
+    ac.setFontAlignment(self.maxpFLValue, "left")
+    ac.setFontAlignment(self.maxpFRValue, "right")
+    ac.setFontAlignment(self.maxpRLValue, "left")
+    ac.setFontAlignment(self.maxpRRValue, "right")
+    
+    ##set font size
+    
+    ac.setFontSize(self.maxFont, 12)
+    ac.setFontSize(self.maxtFont, 12)
+    ac.setFontSize(self.maxpFont, 12)
+    ac.setFontSize(self.maxtFontBottom, 12)
+    ac.setFontSize(self.maxpFontBottom, 12)
+    ac.setFontSize(self.maxtFLValue, 12)
+    ac.setFontSize(self.maxtFRValue, 12)  
+    ac.setFontSize(self.maxtRLValue, 12)
+    ac.setFontSize(self.maxtRRValue, 12)
+    ac.setFontSize(self.maxpFLValue, 12)
+    ac.setFontSize(self.maxpFRValue, 12)
+    ac.setFontSize(self.maxpRLValue, 12)
+    ac.setFontSize(self.maxpRRValue, 12)
+    
+  def setTemp(self, tFL, tFR, tRL, tRR):
+    ac.setText(self.tFLValue, "{0} {1}".format(round(tFL), self.unitChar))
+    ac.setText(self.tFRValue, "{0} {1}".format(round(tFR), self.unitChar))
+    ac.setText(self.tRLValue, "{0} {1}".format(round(tRL), self.unitChar))
+    ac.setText(self.tRRValue, "{0} {1}".format(round(tRR), self.unitChar))
+  
+  def setPressure(self, pFL, pFR, pRL, pRR):
+    ac.setText(self.pFLValue, "{0} psi".format(round(pFL)))
+    ac.setText(self.pFRValue, "{0} psi".format(round(pFR)))
+    ac.setText(self.pRLValue, "{0} psi".format(round(pRL)))
+    ac.setText(self.pRRValue, "{0} psi".format(round(pRR)))
+  
+  def setDirt(self, dFL, dFR, dRL, dRR):
+    ac.setText(self.dFLValue, "{0}%".format(round(dFL*20)))
+    ac.setText(self.dFRValue, "{0}%".format(round(dFR*20)))
+    ac.setText(self.dRLValue, "{0}%".format(round(dRL*20)))
+    ac.setText(self.dRRValue, "{0}%".format(round(dRR*20)))
+  
+  def setWear(self, wFL, wFR, wRL, wRR):
+    ac.setText(self.dFLValue, "{0}%".format(round(wFL)))
+    ac.setText(self.dFRValue, "{0}%".format(round(wFR)))
+    ac.setText(self.dRLValue, "{0}%".format(round(wRL)))
+    ac.setText(self.dRRValue, "{0}%".format(round(wRR)))
+  
+  def setMaxT(self, tFL, tFR, tRL, tRR):
+    if self.maxtFL < tFL:
+      self.maxtFL = tFL
+      ac.setText(self.maxtFLValue, "{0}".format(round(self.maxtFL)))
+    if self.maxtFR < tFR:
+      self.maxtFR = tFR
+      ac.setText(self.maxtFRValue, "{0}".format(round(self.maxtFR)))
+    if self.maxtRL < tRL:
+      self.maxtRL = tRL
+      ac.setText(self.maxtRLValue, "{0}".format(round(self.maxtRL)))
+    if self.maxtRR < tRR:
+      self.maxtRR = tRR
+      ac.setText(self.maxtRRValue, "{0}".format(round(self.maxtRR)))
+
+  def setMaxP(self, pFL, pFR, pRL, pRR):
+    if self.maxpFL < pFL:
+      self.maxpFL = pFL
+      ac.setText(self.maxpFLValue, "{0}".format(round(self.maxpFL)))
+    if self.maxpFR < pFR:
+      self.maxpFR = pFR
+      ac.setText(self.maxpFRValue, "{0}".format(round(self.maxpFR)))
+    if self.maxpRL < pRL:
+      self.maxpRL = pRL
+      ac.setText(self.maxpRLValue, "{0}".format(round(self.maxpRL)))
+    if self.maxpRR < pRR:
+      self.maxpRR = pRR
+      ac.setText(self.maxpRRValue, "{0}".format(round(self.maxpRR)))
+
+def acMain(ac_version):
+  global TYREINFO, optimal_spinner_id, optimal_spinner_shown
+  appWindow = ac.newApp("Tyre Temperatures")
+  ac.drawBackground(appWindow, background)  
+  ac.drawBorder(appWindow, background)
+  ac.setBackgroundOpacity(appWindow, background)
+  ac.setSize(appWindow, x_app_size, y_app_size)
+  TYREINFO = Tyre_Info(appWindow, x_start, y_start)
+  ac.addRenderCallback(appWindow, onFormRender)
+  
+  TYREINFO.optimal_temp = d_optimal_temp
+  TYREINFO.carinifilepath = inidir + getValidFileName(ac.getCarName(0)) + ".ini"
+  TYREINFO.needwriteini = 1
+  
+  if os.path.exists(TYREINFO.carinifilepath):
+    f = open(TYREINFO.carinifilepath, "r")
+    TYREINFO.optimal_temp = int(f.readline()[8:])
+    f.close()
+    TYREINFO.needwriteini = 0
+  
+  optimal_spinner_id = ac.addSpinner(appWindow, "optimal")
+  ac.setPosition(optimal_spinner_id, 0, y_app_size + 28)
+  ac.setStep(optimal_spinner_id, 1)
+  if inFahrenheit:
+    ac.setRange(optimal_spinner_id, int(CelsiusToFahrenheit(50)), int(CelsiusToFahrenheit(150)))
+    ac.setValue(optimal_spinner_id, int(CelsiusToFahrenheit(TYREINFO.optimal_temp)))
+  else:
+    ac.setRange(optimal_spinner_id, 50, 150)
+    ac.setValue(optimal_spinner_id, TYREINFO.optimal_temp)
+  ac.addOnValueChangeListener(optimal_spinner_id, onValueChanged)
+  optimal_spinner_shown = 1
+  
+  ac.log("Danny Giusa Tyre Temperatures loaded")
+  return "Danny Giusa Tyre Temperatures"
+
+def acShutdown():
+  global TYREINFO
+  if not os.path.exists(inidir):
+    os.makedirs(inidir)
+  if TYREINFO.needwriteini == 1:
+    f = open(TYREINFO.carinifilepath, "w")
+    f.write("optimal={0}".format(TYREINFO.optimal_temp))
+    f.close()
+
+def onValueChanged(value):
+  global TYREINFO
+  if inFahrenheit:
+    TYREINFO.optimal_temp = int(FahrenheitToCelsius(value))
+  else:
+    TYREINFO.optimal_temp = value
+  TYREINFO.needwriteini = 1
+
+def onFormRender(deltaT):
+  global TYREINFO, optimal_spinner_id, optimal_spinner_shown
+  tFL, tFR, tRL, tRR = ac.getCarState(0, acsys.CS.CurrentTyresCoreTemp)
+  pFL, pFR, pRL, pRR = ac.getCarState(0, acsys.CS.DynamicPressure)
+  dFL, dFR, dRL, dRR = ac.getCarState(0, acsys.CS.TyreDirtyLevel)
+  wFL, wFR, wRL, wRR = readTyreWear()
+  drawTyresAll(w_tyre, h_tyre, round(tFL, 4), round(tFR, 4), round(tRL, 4), round(tRR, 4), dFL, dFR, dRL, dFR)
+  if inFahrenheit:
+    tFL = CelsiusToFahrenheit(tFL)
+    tFR = CelsiusToFahrenheit(tFR)
+    tRL = CelsiusToFahrenheit(tRL)
+    tRR = CelsiusToFahrenheit(tRR)
+  TYREINFO.setTemp(tFL, tFR, tRL, tRR)
+  TYREINFO.setPressure(pFL, pFR, pRL, pRR)
+  #TYREINFO.setDirt(dFL, dFR, dRL, dRR)
+  TYREINFO.setWear(wFL, wFR, wRL, wRR)
+  TYREINFO.setMaxT(tFL, tFR, tRL, tRR)
+  TYREINFO.setMaxP(pFL, pFR, pRL, pRR)
+  
+  isInPit = readIsInPit()
+  if isInPit == 1 and optimal_spinner_shown == 0:
+    ac.setVisible(optimal_spinner_id, 1)
+    optimal_spinner_shown = 1
+  elif isInPit == 0 and optimal_spinner_shown == 1:
+    ac.setVisible(optimal_spinner_id, 0)
+    optimal_spinner_shown = 0
+
+def readTyreWear():
+  shmHandle = mmap.mmap(0, 256, "acpmf_physics")
+  shmHandle.seek(30*4)
+  data = shmHandle.read(4*4)
+  shmHandle.close()  
+  return struct.unpack("<ffff", data)
+
+def readIsInPit():
+  shmHandle = mmap.mmap(0, 262, "acpmf_graphics")
+  shmHandle.seek(160)
+  isInPit = struct.unpack("<L", shmHandle.read(4))[0]
+  shmHandle.close()  
+  return isInPit
+
+def drawTyresAll(w, h, tFL, tFR, tRL, tRR, dFL, dFR, dRL, dRR):
+  drawTyres(x_tyre_position, y_tyre_position, w, h, tFL)
+  drawTyres(x_tyre_position + x_space - x_tyre_space, y_tyre_position, w, h, tFR)
+  drawTyres(x_tyre_position, y_tyre_position + y_space, w, h, tRL)
+  drawTyres(x_tyre_position + x_space - x_tyre_space, y_tyre_position + y_space, w, h, tRR)
+  drawTyresDirt(x_tyre_position + x_tyredirt_space, y_tyre_position + h_tyre, w_tyredirt, 0, dFL)
+  drawTyresDirt(x_tyre_position + x_space - x_tyre_space + x_tyredirt_space, y_tyre_position + h_tyre, w_tyredirt, 0, dFR)
+  drawTyresDirt(x_tyre_position + x_tyredirt_space, y_tyre_position + y_space + h_tyre, w_tyredirt, 0, dRL)
+  drawTyresDirt(x_tyre_position + x_space - x_tyre_space + x_tyredirt_space, y_tyre_position + y_space + h_tyre, w_tyredirt, 0, dRR)
+
+def drawTyres(x, y, w, h, temp):
+  colorTyres(temp)
+  ac.glQuad(x, y, w, h)
+ 
+def drawTyresDirt(x, y, w, h, dirt):
+  ac.glColor4f(0.9, 0.6, 0.2, 1)
+  ac.glQuad(x, y, w, -(10 * dirt))
+   
+def colorTyres(temp):
+  global TYREINFO
+  hue = (TYREINFO.optimal_temp + 0.5 * temp_range - temp) / temp_range * 240
+  if hue < 0: hue = 0
+  elif hue > 240: hue = 240 
+  r, g, b = hsv2rgb(hue, 1, 0.8)
+  ac.glColor4f(r, g, b, 1)
+
+def hsv2rgb(h, s, v):
+  h = float(h)
+  s = float(s)
+  v = float(v)
+  h60 = h / 60.0
+  h60f = math.floor(h60)
+  hi = int(h60f) % 6
+  f = h60 - h60f
+  p = v * (1 - s)
+  q = v * (1 - f * s)
+  t = v * (1 - (1 - f) * s)
+  r, g, b = 0, 0, 0
+  if hi == 0: r, g, b = v, t, p
+  elif hi == 1: r, g, b = q, v, p
+  elif hi == 2: r, g, b = p, v, t
+  elif hi == 3: r, g, b = p, q, v
+  elif hi == 4: r, g, b = t, p, v
+  elif hi == 5: r, g, b = v, p, q  
+  return r, g, b
+
+def CelsiusToFahrenheit(c):
+  return c * 9 / 5 + 32
+
+def FahrenheitToCelsius(f):
+  return (f - 32) * 5 / 9
+
+def getValidFileName(filename):
+  return "".join(c for c in filename if c in validFilenameChars)
