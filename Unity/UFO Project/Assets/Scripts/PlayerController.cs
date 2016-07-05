181a02f74265616752bd448b5bb40f82df24df78
Troy Uyan <blutank24@msn.com>
2016-07-04 22:10:09 -0700

    UFO vid up to 3

---
+++ Unity/UFO Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// shows up in editor
+	// make changes in editor
+	public float speed;
+
+	// get the rb2d component
+	private Rigidbody2D rb2d;
+
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	// apply physics per frame
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.AddForce (movement * speed);
+	}
+}
